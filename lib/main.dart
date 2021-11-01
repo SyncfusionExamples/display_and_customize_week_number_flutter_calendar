c823e41269f2e27d19e5f5a6c1ce534da02bfac7
arunsiva.rengaraju <arunsiva.rengaraju@syncfusion.com>
2021-11-01 17:25:49 +0530

    Sample added.

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(const WeekNumber());
+
+class WeekNumber extends StatefulWidget {
+  const WeekNumber({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => WeekNumberCustomization();
+}
+
+class WeekNumberCustomization extends State<WeekNumber> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: SafeArea(
+        child: SfCalendar(
+          showWeekNumber: true,
+          view: CalendarView.month,
+          allowedViews: const[
+            CalendarView.day,
+            CalendarView.week,
+            CalendarView.workWeek,
+            CalendarView.month,
+            CalendarView.timelineDay,
+            CalendarView.timelineWeek,
+            CalendarView.timelineWorkWeek,
+            CalendarView.timelineMonth
+          ],
+          weekNumberStyle: const WeekNumberStyle(
+              textStyle: TextStyle(color: Colors.amber),
+              backgroundColor: Color(0xFFDF5E5E)),
+        ),
+      )),
+    );
+  }
+}
